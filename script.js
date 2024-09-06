81796ed0f0b4df07aeb600343167c9f2b1db721e
Lucas Eineck Machado <132614631+LucasEineckMachado@users.noreply.github.com>
2024-09-06 10:43:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.getElementById('message2').addEventListener('click', function() {
 });
 
 document.getElementById('message3').addEventListener('click', function() {
-    document.getElementById('output').textContent = 'Olha esse olhar de boiola!';
+    document.getElementById('output').textContent = 'Seu sorriso ilumina o meu dia!';
     document.getElementById('defaultGif').classList.add('hidden');
     document.getElementById('gif1').classList.add('hidden');
     document.getElementById('gif2').classList.add('hidden');
