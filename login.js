e5e5ad0dae01de1588ad654fda2ee1e82573bbee
Lucas Eineck Machado <132614631+LucasEineckMachado@users.noreply.github.com>
2024-09-08 14:27:57 -0300

    Atualizando modificações nos arquivos HTML, CSS e Java

---
+++ login.js
@@ -0,0 +1,13 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Verificação simples (deve ser feita no servidor para mais segurança)
+    if (username === 'Lyt28383' && password === '05052005') {
+        window.location.href = 'index.html'; // Redireciona para a página principal
+    } else {
+        document.getElementById('loginError').classList.remove('hidden');
+    }
+});
